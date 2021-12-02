847347b8b6f34293fb6d504777311f352a4e1cf5
WojciechWirowski <71640545+WojciechWirowski@users.noreply.github.com>
2021-12-02 14:50:35 +0100

    RecordsRepository- I still have to finish more methods, but I had to leave to work  TODO:  -testing RecordsRepository and WorkingScheduleRepository  -method in RecordsRepository for getting data from whole month from "some day" since "now"  -

--- EASV.CS20B.FW.WorkScheduleProject.Database/Entities/RecordEntity.cs
+++ EASV.CS20B.FW.WorkScheduleProject.Database/Entities/RecordEntity.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace EASV.CS20B.FW.WorkScheduleProject.Database.Entities
 {
     public class RecordEntity
     {
-        
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int TimeIn { get; set; }
+        public int TimeOut { get; set; }
+        public int UserId { get; set; }
     }
 }
--- EASV.CS20B.FW.WorkScheduleProject.Database/Repositories/RecordsRepository.cs
+++ EASV.CS20B.FW.WorkScheduleProject.Database/Repositories/RecordsRepository.cs
@@ -1,7 +1,86 @@
+using System.Collections.Generic;
+using System.Linq;
+using EASV.CS20B.FW.WorkScheduleProject.Database.Entities;
+using Xunit;
+
 namespace EASV.CS20B.FW.WorkScheduleProject.Database.Repositories
 {
     public class RecordsRepository
     {
+        private readonly ScheduleApplicationContext _ctx;
+
+        public RecordsRepository(ScheduleApplicationContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public List<RecordEntity> GetAllRecords()
+        {
+            var selectQuery = _ctx.Records.Select(recordEntity => new RecordEntity()
+            {
+                Id = recordEntity.Id,
+               Date = recordEntity.Date,
+               TimeIn = recordEntity.TimeIn,
+               TimeOut = recordEntity.TimeOut,
+               UserId = recordEntity.UserId
+            });
+            return selectQuery.ToList();
+        }
+
+        public RecordEntity CreateRecord(RecordEntity record)
+        {
+            RecordEntity recordEntity = new RecordEntity
+            {
+                Id = record.Id,
+                Date = record.Date,
+                TimeIn = record.TimeIn,
+                TimeOut = record.TimeOut,
+                UserId = record.UserId
+            };
+            _ctx.Records.Add(recordEntity);
+            _ctx.SaveChanges();
+            return record;
+        }
+
+        public RecordEntity Modify(RecordEntity record)
+        {
+            RecordEntity recordEntity = new RecordEntity
+            {
+                Id = record.Id,
+                Date = record.Date,
+                TimeIn = record.TimeIn,
+                TimeOut = record.TimeOut,
+                UserId = record.UserId
+            };
+            _ctx.Records.Update(recordEntity);
+            _ctx.SaveChanges();
+            return record;
+        }
         
+        public RecordEntity Delete(int id)
+        {
+            var entity = _ctx.Records.Remove(new RecordEntity {Id = id}).Entity;
+            _ctx.SaveChanges();
+            return new RecordEntity
+            {
+                Id = entity.Id,
+                Date = entity.Date,
+                TimeIn = entity.TimeIn,
+                TimeOut = entity.TimeOut,
+                UserId = entity.UserId
+            };
+        }
+
+        public RecordEntity GetById(int id)
+        {
+            var selectQuery = _ctx.Records.Select(recordEntity => new RecordEntity()
+            {
+                Id = recordEntity.Id,
+                Date = recordEntity.Date,
+                TimeIn = recordEntity.TimeIn,
+                TimeOut = recordEntity.TimeOut,
+                UserId = recordEntity.UserId
+            }).FirstOrDefault(r => r.Id == id);
+            return selectQuery;
+        }
     }
 }
